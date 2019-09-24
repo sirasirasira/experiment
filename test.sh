6060400f7557ca9e1ff13b2b6e9ae387071f05c9
sira <sira@ist.hokudai.ac.jp>
2019-09-25 08:42:22 +0900

    comment out

--- test.sh
+++ test.sh
@@ -22,24 +22,24 @@ mkdir ./result/${data}
 #for idx in 0 1 2 3 4 5 6 7 8 9
 for idx in 0 1
 do
-	./bin/lpboost_correct -m 1 -x 10 -n 0.5 ./data/${data}_buckets/train${idx}.gsp
+	./bin/lpboost_correct -m 1 -x 10 -n 0.5 ./data/${data}_buckets/train${idx}.gsp > ./result/${data}/tmp1
 	./bin/evaluator ./model ./data/${data}_buckets/test${idx}.gsp
 done
 
 #gtb correct
 for idx in 0 1 2 3 4 5 6 7 8 9
 do
-	./bin/gtb_correct -m 1 -x 2 -d 3 -t 10 ./data/${data}_buckets/train${idx}.gsp ./data/${data}_buckets/test${idx}.gsp >> ./result/${data}/tmp1
+	./bin/gtb_correct -m 1 -x 2 -d 3 -t 10 ./data/${data}_buckets/train${idx}.gsp ./data/${data}_buckets/test${idx}.gsp > ./result/${data}/tmp1
 	echo "#${idx} model" >> ./result/${data}/tmp2
-	tail -n 11 ./result/${data}/tmp2 | head -n 10 >> ./result/${data}/tmp2
+	tail -n 11 ./result/${data}/tmp1 | head -n 10 >> ./result/${data}/tmp2
 done
 python ./bin/gtb_mean.py ./result/${data}/tmp2 >> ./result/${data}/gtb_correct
 
 #gtb bandit
 for idx in 0 1 2 3 4 5 6 7 8 9
 do
-	./bin/gtb_bandit -m 1 -x 2 -d 3 -t 10 -i 50 ./data/${data}_buckets/train${idx}.gsp ./data/${data}_buckets/test${idx}.gsp >> ./result/${data}/tmp1
+	./bin/gtb_bandit -m 1 -x 2 -d 3 -t 10 -i 50 ./data/${data}_buckets/train${idx}.gsp ./data/${data}_buckets/test${idx}.gsp > ./result/${data}/tmp1
 	echo "#${idx} model" >> ./result/${data}/tmp2
-	tail -n 11 ./result/${data}/tmp2 | head -n 10 >> ./result/${data}/tmp2
+	tail -n 11 ./result/${data}/tmp1 | head -n 10 >> ./result/${data}/tmp2
 done
 python ./bin/gtb_mean.py ./result/${data}/tmp2 >> ./result/${data}/gtb_bandit
